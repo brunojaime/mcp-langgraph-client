5dd34182b011ed81f1b088ae6145c4376d629ac7
Bruno Jaime <bjaime@frba.utn.edu.ar>
2025-05-20 21:33:34 -0300

    Refactor main.py to include HumanMessage import, correct math server path, and implement async session initialization with LLM agent invocation for basic math query.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import asyncio
 import os
-
+from langchain_core.messages import HumanMessage
 from dotenv import load_dotenv
 from langchain_mcp_adapters.tools import load_mcp_tools
 from langchain_openai import ChatOpenAI
@@ -12,15 +12,24 @@ load_dotenv()
 
 llm = ChatOpenAI()
 
+#Information on how to run the mcp-server:
 stdio_server_params = StdioServerParameters(
 command="python",
-args=["/home/brunojaime/Documents/Projects/mcp-langgraph-client/servers/math_server.py"],
+args=["/home/brunojaime/Documents/Projects/mcp-langgraph-client/servers/math_servers.py"],
 
 )
 
 async def main():
-    print("Hello from mcp-langgraph-client!")
-    
+    async with stdio_client(stdio_server_params) as (read,write):   
+        async with ClientSession(read_stream=read,write_stream=write) as session:
+            await session.initialize()
+            print("session initialized")
+            tools = await load_mcp_tools(session)
+            print(tools)
+
+            agent = create_react_agent(llm,tools)
+            result = await agent.ainvoke({"messages":[HumanMessage(content="What is 2+2?")]})
+            print(result["messages"][-1].content)
 
 if __name__ == "__main__":
     asyncio.run(main())
